ac2cd5c511ce15ce074fbdaff36951b23a0673ac
Simon Blasen <Simon.yzf@t-online.de>
2021-06-29 19:07:51 +0200

    Cloud shader script X and Y

--- Locomotive/Assets/Scripts/Shader/CloudShaderSun.cs
+++ Locomotive/Assets/Scripts/Shader/CloudShaderSun.cs
@@ -9,7 +9,9 @@ public class CloudShaderSun : MonoBehaviour
     [SerializeField]
     private MeshRenderer cloudShaderMeshRenderer = null;
     [SerializeField]
-    private string shaderPropertyID = "";
+    private string shaderPropertyXID = "";
+    [SerializeField]
+    private string shaderPropertyYID = "";
     [SerializeField]
     private Transform sunDirectionalLight = null;
 
@@ -27,7 +29,16 @@ public class CloudShaderSun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        matCopy.SetFloat(shaderPropertyID, make0_360(sunDirectionalLight.rotation.eulerAngles.y));
+        float angle = transform.rotation.eulerAngles.x;
+
+        float sin = Mathf.Sin(angle * Mathf.PI / 180f);
+        float cos = Mathf.Cos(angle * Mathf.PI / 180f);
+
+        matCopy.SetFloat(shaderPropertyXID, sin);
+        matCopy.SetFloat(shaderPropertyYID, cos);
+
+        Debug.Log("Sin: " + sin.ToString("n2"));
+        Debug.Log("Cos: " + cos.ToString("n2"));
     }
 
     private float make0_360(float angleVal)
