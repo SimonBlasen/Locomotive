32b70492c7bb73bdb820338a66fea59105f00222
Simon Blasen <Simon.yzf@t-online.de>
2021-09-11 19:48:47 +0200

    Worked on Railroad Map

--- Locomotive/Assets/Scripts/Player/RailroadMapMover.cs
+++ Locomotive/Assets/Scripts/Player/RailroadMapMover.cs
@@ -1,3 +1,4 @@
+using SappAnims;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,33 @@ using UnityEngine;
 public class RailroadMapMover : MonoBehaviour
 {
     [SerializeField]
-    private SappAnim
+    private SappAnim animMovingMap = null;
+    [SerializeField]
+    private Transform movingMapBackPos = null;
+    [SerializeField]
+    private Transform movingMapFrontPos = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animMovingMap.transform.localPosition = movingMapBackPos.localPosition;
+        animMovingMap.transform.localRotation = Quaternion.Euler(movingMapBackPos.localRotation.eulerAngles);
+        animMovingMap.LocalPosition = movingMapBackPos.localPosition;
+        animMovingMap.LocalRotation = movingMapBackPos.localRotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            animMovingMap.LocalPosition = movingMapFrontPos.localPosition;
+            animMovingMap.LocalRotation = movingMapFrontPos.localRotation.eulerAngles;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            animMovingMap.LocalPosition = movingMapBackPos.localPosition;
+            animMovingMap.LocalRotation = movingMapBackPos.localRotation.eulerAngles;
+        }
     }
 }
