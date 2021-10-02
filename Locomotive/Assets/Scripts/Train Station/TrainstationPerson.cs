833ea5504180a07360a100ec33e9502b60c486d0
Simon Blasen <Simon.yzf@t-online.de>
2021-10-02 18:16:10 +0200

    Added another TrainStation

--- Locomotive/Assets/Scripts/Train Station/TrainstationPerson.cs
+++ Locomotive/Assets/Scripts/Train Station/TrainstationPerson.cs
@@ -12,6 +12,7 @@ public class TrainstationPerson : MonoBehaviour
 
     private float enableNavMeshAgent = 0.05f;
 
+    // TODO: Ticks on train needs to only work, if agent doesn't move anymore
     private int ticksOnTrainArea = 0;
 
 
@@ -34,21 +35,23 @@ public class TrainstationPerson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        randomWalkTimer -= Time.deltaTime;
-        if (randomWalkTimer <= 0f)
+        if (IsEnteringTrain == false)
         {
-            randomWalkTimer = Random.Range(1f, 15f);
+            randomWalkTimer -= Time.deltaTime;
+            if (randomWalkTimer <= 0f)
+            {
+                randomWalkTimer = Random.Range(1f, 15f);
 
-            if (HasPreviouslyExitedTrain)
-            {
-                navAgent.SetDestination(WaitingPlatform.platformExit.position);
-            }
-            else
-            {
-                //if (navAgent.velocity.magnitude < 0.05f)
-                //{
-                navAgent.SetDestination(Vector3.Lerp(WaitingPlatform.waitingAreaMinPos.position, WaitingPlatform.waitingAreaMaxPos.position, UnityEngine.Random.Range(0f, 1f)));
-                //}
+                if (HasPreviouslyExitedTrain)
+                {
+                }
+                else
+                {
+                    //if (navAgent.velocity.magnitude < 0.05f)
+                    //{
+                    navAgent.SetDestination(Vector3.Lerp(WaitingPlatform.waitingAreaMinPos.position, WaitingPlatform.waitingAreaMaxPos.position, UnityEngine.Random.Range(0f, 1f)));
+                    //}
+                }
             }
         }
 
@@ -58,7 +61,9 @@ public class TrainstationPerson : MonoBehaviour
         {
             if (HasPreviouslyExitedTrain)
             {
-                if (Vector3.Distance(transform.position, navAgent.destination) <= 2f)
+                Vector2 exitPos = new Vector2(WaitingPlatform.waitingAreaMaxPos.position.x, WaitingPlatform.waitingAreaMaxPos.position.z);
+                Vector2 selfPos = new Vector2(transform.position.x, transform.position.z);
+                if (Vector2.Distance(exitPos, selfPos) <= 0.2f)
                 {
                     Destroy(gameObject);
                 }
@@ -75,10 +80,11 @@ public class TrainstationPerson : MonoBehaviour
                     {
                         Debug.Log("Exiting person has existed");
                         IsExitingTrain = false;
+                        navAgent.SetDestination(WaitingPlatform.platformExit.position);
                         CanEnterTrain = false;
                         randomWalkTimer = 1f;
 
-                        Destroy(gameObject, Random.Range(5f, 10f));
+                        Destroy(gameObject, Random.Range(20f, 25f));
                     }
                 }
             }
@@ -87,8 +93,10 @@ public class TrainstationPerson : MonoBehaviour
             {
                 Vector2 flatDest = new Vector2(navAgent.destination.x, navAgent.destination.z);
                 Vector2 flatPos = new Vector2(transform.position.x, transform.position.z);
-                if (Vector2.Distance(flatDest, flatPos) <= 0.2f || ticksOnTrainArea >= 10)
+                // TODO: Ticks on train needs to only work, if agent doesn't move anymore
+                if (Vector2.Distance(flatDest, flatPos) <= 0.2f)// || ticksOnTrainArea >= 10)
                 {
+                    Debug.Log("Person entered waggon");
                     List<TrainStation> possibleTrainstations = new List<TrainStation>();
                     possibleTrainstations.AddRange(TrainStation.AllTrainstations);
                     possibleTrainstations.Remove(OriginTrainstation);
