0ed6b2cfd31ef2b8334e74e6d0a5163a6ff59c6f
Simon Blasen <Simon.yzf@t-online.de>
2021-10-15 13:05:37 +0200

    Added 100km terrain

---
+++ Locomotive/Assets/Scripts/Utils/TerrainMaterialAssigner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TerrainMaterialAssigner : MonoBehaviour
+{
+    public Material toAssignMaterial = null;
+
+    public bool assign = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (assign)
+        {
+            assign = false;
+
+            Terrain[] terrains = FindObjectsOfType<Terrain>();
+
+            for (int i = 0; i < terrains.Length; i++)
+            {
+                terrains[i].materialTemplate = toAssignMaterial;
+            }
+        }
+    }
+}
