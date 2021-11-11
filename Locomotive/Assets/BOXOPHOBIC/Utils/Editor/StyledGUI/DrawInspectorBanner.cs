05bca0e11800867f293da2d3b6f0363d705b6c91
Estalliant <paul.mueller.99@web.de>
2021-11-11 08:29:48 +0100

    2 Height Fog Assets

--- Locomotive/Assets/BOXOPHOBIC/Utils/Editor/StyledGUI/DrawInspectorBanner.cs
+++ Locomotive/Assets/BOXOPHOBIC/Utils/Editor/StyledGUI/DrawInspectorBanner.cs
@@ -6,76 +6,55 @@ using Boxophobic.Constants;
 
 namespace Boxophobic.StyledGUI
 {
-    public partial class StyledGUI
+    public partial class StyledGUI 
     {
-        public static void DrawInspectorBanner(Color color, string title, string help)
+        public static void DrawInspectorBanner(Color bannerColor, string title, string helpURL)
         {
             GUILayout.Space(10);
 
             var bannerFullRect = GUILayoutUtility.GetRect(0, 0, 36, 0);
             var bannerBeginRect = new Rect(bannerFullRect.position.x, bannerFullRect.position.y, 20, 36);
-            var bannerMiddleRect = new Rect(bannerFullRect.position.x + 20, bannerFullRect.position.y, bannerFullRect.xMax - 58, 36);
+            var bannerMiddleRect = new Rect(bannerFullRect.position.x + 20, bannerFullRect.position.y, bannerFullRect.xMax - 54, 36);
             var bannerEndRect = new Rect(bannerFullRect.xMax - 20, bannerFullRect.position.y, 20, 36);
             var iconRect = new Rect(bannerFullRect.xMax - 34, bannerFullRect.position.y + 5, 30, 26);
 
+            Color guiColor;
+
             if (EditorGUIUtility.isProSkin)
             {
-                color = new Color(color.r, color.g, color.b, 1f);
+                bannerColor = new Color(bannerColor.r, bannerColor.g, bannerColor.b, 1f);
             }
             else
             {
-                color = CONSTANT.ColorLightGray;
+                bannerColor = CONSTANT.ColorLightGray;
             }
 
-            GUI.color = color;
+            if (bannerColor.r + bannerColor.g + bannerColor.b <= 1.5f)
+            {
+                guiColor = CONSTANT.ColorLightGray;
+            }
+            else
+            {
+                guiColor = CONSTANT.ColorDarkGray;
+            }
+
+            GUI.color = bannerColor;
 
             GUI.DrawTexture(bannerBeginRect, CONSTANT.BannerImageBegin, ScaleMode.StretchToFill, true);
             GUI.DrawTexture(bannerMiddleRect, CONSTANT.BannerImageMiddle, ScaleMode.StretchToFill, true);
             GUI.DrawTexture(bannerEndRect, CONSTANT.BannerImageEnd, ScaleMode.StretchToFill, true);
 
-            Color guiColor = CONSTANT.ColorDarkGray;
-
+#if UNITY_2019_3_OR_NEWER
             GUI.Label(bannerFullRect, "<size=16><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + ">" + title + "</color></size>", CONSTANT.TitleStyle);
-
+#else
+            GUI.Label(bannerFullRect, "<size=14><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + "><b>" + title + "</b></color></size>", CONSTANT.TitleStyle);
+#endif
             GUI.color = guiColor;
 
             if (GUI.Button(iconRect, CONSTANT.IconHelp, new GUIStyle { alignment = TextAnchor.MiddleCenter }))
             {
-                Application.OpenURL(help);
+                Application.OpenURL(helpURL);
             }
-
-            GUI.color = Color.white;
-
-            GUILayout.Space(10);
-        }
-
-        public static void DrawInspectorBanner(Color color, string title)
-        {
-            GUILayout.Space(10);
-
-            var bannerFullRect = GUILayoutUtility.GetRect(0, 0, 36, 0);
-            var bannerBeginRect = new Rect(bannerFullRect.position.x, bannerFullRect.position.y, 20, 36);
-            var bannerMiddleRect = new Rect(bannerFullRect.position.x + 20, bannerFullRect.position.y, bannerFullRect.xMax - 58, 36);
-            var bannerEndRect = new Rect(bannerFullRect.xMax - 20, bannerFullRect.position.y, 20, 36);
-
-            if (EditorGUIUtility.isProSkin)
-            {
-                color = new Color(color.r, color.g, color.b, 1f);
-            }
-            else
-            {
-                color = CONSTANT.ColorLightGray;
-            }
-
-            GUI.color = color;
-
-            GUI.DrawTexture(bannerBeginRect, CONSTANT.BannerImageBegin, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(bannerMiddleRect, CONSTANT.BannerImageMiddle, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(bannerEndRect, CONSTANT.BannerImageEnd, ScaleMode.StretchToFill, true);
-
-            Color guiColor = CONSTANT.ColorDarkGray;
-
-            GUI.Label(bannerFullRect, "<size=16><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + ">" + title + "</color></size>", CONSTANT.TitleStyle);
 
             GUI.color = Color.white;
 
@@ -111,51 +90,11 @@ namespace Boxophobic.StyledGUI
             GUI.DrawTexture(bannerMiddleRect, CONSTANT.BannerImageMiddle, ScaleMode.StretchToFill, true);
             GUI.DrawTexture(bannerEndRect, CONSTANT.BannerImageEnd, ScaleMode.StretchToFill, true);
 
+#if UNITY_2019_3_OR_NEWER
             GUI.Label(bannerFullRect, "<size=16><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + ">" + title + "</color></size>", CONSTANT.TitleStyle);
-
-            GUI.color = Color.white;
-
-            GUILayout.Space(10);
-        }
-
-        public static void DrawInspectorBanner(string title, string help)
-        {
-            GUILayout.Space(10);
-
-            var bannerFullRect = GUILayoutUtility.GetRect(0, 0, 36, 0);
-            var bannerBeginRect = new Rect(bannerFullRect.position.x, bannerFullRect.position.y, 20, 36);
-            var bannerMiddleRect = new Rect(bannerFullRect.position.x + 20, bannerFullRect.position.y, bannerFullRect.xMax - 58, 36);
-            var bannerEndRect = new Rect(bannerFullRect.xMax - 20, bannerFullRect.position.y, 20, 36);
-            var iconRect = new Rect(bannerFullRect.xMax - 34, bannerFullRect.position.y + 5, 30, 26);
-
-            Color bannerColor;
-            Color guiColor;
-
-            if (EditorGUIUtility.isProSkin)
-            {
-                bannerColor = CONSTANT.ColorDarkGray;
-                guiColor = CONSTANT.ColorLightGray;
-            }
-            else
-            {
-                bannerColor = CONSTANT.ColorLightGray;
-                guiColor = CONSTANT.ColorDarkGray;
-            }
-
-            GUI.color = bannerColor;
-
-            GUI.DrawTexture(bannerBeginRect, CONSTANT.BannerImageBegin, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(bannerMiddleRect, CONSTANT.BannerImageMiddle, ScaleMode.StretchToFill, true);
-            GUI.DrawTexture(bannerEndRect, CONSTANT.BannerImageEnd, ScaleMode.StretchToFill, true);
-
-            GUI.Label(bannerFullRect, "<size=16><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + ">" + title + "</color></size>", CONSTANT.TitleStyle);
-
-            GUI.color = guiColor;
-
-            if (GUI.Button(iconRect, CONSTANT.IconHelp, new GUIStyle { alignment = TextAnchor.MiddleCenter }))
-            {
-                Application.OpenURL(help);
-            }
+#else
+            GUI.Label(bannerFullRect, "<size=14><color=#" + ColorUtility.ToHtmlStringRGB(guiColor) + "><b>" + title + "</b></color></size>", CONSTANT.TitleStyle);
+#endif
 
             GUI.color = Color.white;
 
