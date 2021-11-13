007ff24d62f66550b9cdd8b283ce650f128a2ae3
Simon Blasen <Simon.yzf@t-online.de>
2021-11-13 10:05:14 +0100

    Added rails to Game scene

---
+++ Locomotive/Assets/Scripts/Procedural Generation/Environment/ProcEnvSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcEnvSpawner : MonoBehaviour
+{
+    public bool active = false;
+
+    [Space]
+
+    public bool generateObjects = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
